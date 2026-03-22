426f4ba7455de0b421586311c42747ce18071874
pheineke <heineke.pe@gmail.com>
2026-03-22 20:35:48 +0100

    chore: clean up temp scripts and finalize PWA icons

--- fix_react.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const p = '/home/pi/hub/frontend/src/apps/media_downloader/index.tsx';
-let code = fs.readFileSync(p, 'utf8');
-
-// Wait... let's just make the changes directly.
